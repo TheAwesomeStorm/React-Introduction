2607af951abeba291066594fe9eb320262fc846b
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-12-05 21:18:32 -0300

    Created List Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Forms } from './components/forms/forms'
+import { List } from './components/list/list'
 
 function App() {
   return (
     <div className="App">
       <Forms />
+      <List />
     </div>
   );
 }
