a54137f0ef480018cfa19120bebd45bc414dd358
Daniel Mendes Sechinske <daniel.sechinske@guiainvest.com.br>
2021-12-10 08:38:10 -0300

    Added useState to List component

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './list.module.scss'
 import { Item } from './item/item'
 
 export function List() {
-  const tasks = [{
+  const [tasks, setTasks] = useState([{
     tarefa: 'Angular',
     tempo: '02:00:00'
   }, {
@@ -12,11 +12,15 @@ export function List() {
   }, {
     tarefa: 'TypeScript',
     tempo: '00:00:30'
-  }]
+  }])
+
+  function AddTask() {
+    setTasks([...tasks, {tarefa: 'Estudar state', tempo: '05:00:00'}])
+  }
 
   return (
     <aside className={style.taskList}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={AddTask}>Estudos do dia</h2>
       <ul>
         {tasks.map((item, index) => (
           <Item key={index} {...item} />
